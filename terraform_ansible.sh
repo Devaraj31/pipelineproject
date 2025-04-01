48c9ab04c57198c3da460da67dcfda767fb77317
Devaraj31 <73634723+Devaraj31@users.noreply.github.com>
2025-04-01 14:32:35 +0530

    Create terraform_ansible.sh

---
+++ terraform_ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common -y
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform -y
+
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
